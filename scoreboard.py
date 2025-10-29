476d7640ea0a65ca8c792156304e0f4a27160546
Des Whelan <wheland6@tcd.ie>
2025-10-29 10:14:11 +0000

    persist high score across sessions

--- scoreboard.py
+++ scoreboard.py
@@ -9,12 +9,19 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_high_score()
         self.color(COLOUR)
         self.hideturtle()
         self.penup()
         self.setpos(0, 275)
         self.display_current_score()
+
+    @staticmethod
+    def get_high_score():
+        """Retrieves and returns the high score from the high score log"""
+        with open("high_score_log.txt") as high_score_log:
+            high_score = int(high_score_log.read())
+        return high_score
 
     def display_current_score(self):
         self.clear()
@@ -27,6 +34,7 @@ class Scoreboard(Turtle):
     def reset_score(self):
         if self.score > self.high_score:
             self.high_score = self.score
-
+            with open("high_score_log.txt", "w") as high_score_log:
+                high_score_log.write(str(self.score))
         self.score = 0
         self.display_current_score()
